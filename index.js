cbe57a8d7bc4beea5a831d1e1e54ad120ae3bfac
utimur <akva_ten@mail.ru>
2020-11-17 21:36:47 +0300

    5 lesson server sockets

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const WSServer = require('express-ws')(app)
+const aWss = WSServer.getWss()
+
+const PORT = process.env.PORT || 5000
+
+app.ws('/', (ws, req) => {
+    ws.on('message', (msg) => {
+        msg = JSON.parse(msg)
+        switch (msg.method) {
+            case "connection":
+                connectionHandler(ws, msg)
+                break
+        }
+    })
+})
+
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
+
+const connectionHandler = (ws, msg) => {
+    ws.id = msg.id
+    broadcastConnection(ws, msg)
+}
+
+const broadcastConnection = (ws, msg) => {
+    aWss.clients.forEach(client => {
+        if (client.id === msg.id) {
+            client.send(`Пользователь ${msg.username} подключился`)
+        }
+    })
+}
